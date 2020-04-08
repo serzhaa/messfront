dfce86a69b49c6d05555bbaace24c67dc9c27330
serzhaa <62075472+serzhaa@users.noreply.github.com>
2020-04-08 05:01:00 +0300

    write header and navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navigation">
+      <a href="#" className="navigation__item">My profile</a>
+      <a href="#" className="navigation__item">Messages</a>
+      <a href="#" className="navigation__item">News</a>
+      <a href="#" className="navigation__item">Music</a>
+      <a href="#" className="navigation__item">Settings</a>
+    </nav>
+  );
+}
+
+export default Navbar;
